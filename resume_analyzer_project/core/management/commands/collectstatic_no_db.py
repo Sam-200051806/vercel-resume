46fdca52553fa017a7a622b2afb1daf1a94813f8
Sam-200051806 <sambhavseth1806@gmail.com>
2025-05-21 00:42:32 +0530

    Fix collectstatic_no_db import path

--- resume_analyzer_project/core/management/commands/collectstatic_no_db.py
+++ resume_analyzer_project/core/management/commands/collectstatic_no_db.py
@@ -1,4 +1,4 @@
-from django.core.management.commands import collectstatic
+from django.contrib.staticfiles.management.commands import collectstatic
 from django.db import connections
 
 
@@ -7,14 +7,14 @@ class Command(collectstatic.Command):
     A version of collectstatic that doesn't require database access.
     This is useful for environments like Vercel where we don't have a database.
     """
-    
+
     def handle(self, **options):
         # Close any existing database connections
         for conn in connections.all():
             conn.close_if_unusable_or_obsolete()
-        
+
         # Set the --skip-checks option to avoid database checks
         options['skip_checks'] = True
-        
+
         # Call the parent handle method
         return super().handle(**options)
